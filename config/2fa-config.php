067e7903790a0135913e41ba473e5cf99f2ed41a
Ashish Singh <imrealashu@gmail.com>
2017-03-19 02:14:26 +0530

    Resolved: #12 - Add configurable option to specify digest algorithm and length

--- config/2fa-config.php
+++ config/2fa-config.php
@@ -14,6 +14,18 @@ return [
      */
     'account_name' => 'Thecodework 2FA',
 
+    /*
+     * Currntly Support 'Sha1'
+     * The library works with the Google Authenticator application
+     * for iPhone and Android. Google only supports SHA-1 digest algorithm,
+     * 30 second period and 6 digits OTP. Other values for these parameters
+     * are ignored by the Google Authenticator application.
+     */
     'digest_algorithm' => 'sha1',
+
+    /*
+     * Number of digits can be max 30
+     * To Support Google Authenticator
+     */
     'number_of_digits' => 8
 ];
