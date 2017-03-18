72e0fe868f41b480f94db5443cd700d4d2aca935
Ashish Singh <imrealashu@gmail.com>
2017-03-19 00:12:04 +0530

    Little updation in the internal docs. Now instead of manually checking for valid token now defined expilicit validation rule. #7

--- config/2fa-config.php
+++ config/2fa-config.php
@@ -1,6 +1,16 @@
 <?php
 
 return [
+
+    /*
+     * Specify redirect url after when token authentication
+     * is successfull.
+     */
     'redirect_to' => '/home',
+
+    /*
+     * Account name which will be used as label to show on
+     * authenticator mobile application.
+     */
     'account_name' => 'Thecodework 2FA'
 ];
--- src/AuthenticatesUsersWith2FA.php
+++ src/AuthenticatesUsersWith2FA.php
@@ -10,6 +10,20 @@ use Validator;
 
 trait AuthenticatesUsersWith2Fa
 {
+    /*
+     * Priveate variable to store user object.
+     */
+    private $user;
+
+    /**
+     * If username/password is authenticated then the authenticted.
+     * If 2FA enabled it will redirect user to enter TOTP Token else
+     * Logs the user in normally
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \App\User $user
+     * @return \Illuminate\Http\RedirectResponse
+     */
     protected function authenticated(Request $request, $user)
     {
         if ($user->is_2fa_enabled) {
@@ -19,31 +33,50 @@ trait AuthenticatesUsersWith2Fa
         }
         return redirect()->intended(config('2fa-config.redirect_to'));
     }
+
+    /**
+     * Verify token and sign in the user.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
     public function verifyToken(Request $request)
     {
-        $userId = $request->session()->get('2fa:user:id');
-        $user = User::find(decrypt($userId));
+        # Pulling encrypted user id from session and getting user details
+        $userId     = $request->session()->get('2fa:user:id');
+        $this->user = User::find(decrypt($userId));
 
+        # If token is not valid then custom validation error message will be shown.
+        $messages = [
+            'totp_token.valid_token' => 'Token is not valid',
+        ];
+
+        # Impllicitly adding an validation rule to check if token is valid or not.
+        Validator::extendImplicit('valid_token', function ($attribute, $value, $parameters, $validator) {
+            $totp = new TOTP(
+                config('2fa-config.account_name'),
+                $this->user->secret_key
+            );
+            return $value == $totp->now();
+        });
+
+        # If Validation fails, it will return the error else sign in the user.
         $validator = Validator::make($request->all(), [
-            'totp_token' => 'required|digits:6',
-        ]);
+            'totp_token' => 'required|digits:6|valid_token',
+        ], $messages);
+
         if ($validator->fails()) {
             return redirect('verify-2fa')
                 ->withErrors($validator)
                 ->withInput();
-        }
-
-        $totp = new TOTP(
-            config('2fa-config.account_name'),
-            $user->secret_key
-        );
-
-        if ($totp->now() === $request->totp_token) {
-            $request->session()->forget('2fa:user:id');
-            Auth::loginUsingId($user->id);
-            return redirect()->intended(config('2fa-config.redirect_to'));
         } else {
 
         }
+
+        # Flush the session.
+        $request->session()->forget('2fa:user:id');
+
+        Auth::loginUsingId($this->user->id);
+        return redirect()->intended(config('2fa-config.redirect_to'));
     }
 }
