43a9233fd02d9b9a03e48b6ae57e9c0f00d6c960
Ashish Singh <imrealashu@gmail.com>
2017-03-20 08:05:40 +0000

    Apply fixes from StyleCI

--- src/Contracts/TwoFactorAuthenticationInterface.php
+++ src/Contracts/TwoFactorAuthenticationInterface.php
@@ -4,12 +4,11 @@ namespace Thecodework\TwoFactorAuthentication\Contracts;
 
 use Illuminate\Http\Request;
 
-interface TwoFactorAuthenticationInterface {
-
+interface TwoFactorAuthenticationInterface
+{
     public function setupTwoFactorAuthentication(Request $request);
 
     public function enableTwoFactorAuthentication(Request $request);
 
     public function verifyTwoFactorAuthentication(Request $request);
-
 }
