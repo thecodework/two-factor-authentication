f50d60bea28b83c132504afaf599cbc03600a5cc
Ashish Singh <imrealashu@gmail.com>
2017-03-21 13:18:25 +0530

    One test case added for configuring Travis.CI

--- tests/TwoFactorAuthenticationTest.php
+++ tests/TwoFactorAuthenticationTest.php
@@ -4,6 +4,15 @@ namespace Thecodework\TwoFactorAuthentication\Tests;
 
 class TwoFactorAuthenticationTest extends BaseTestCase
 {
+    /**
+     * Test Users count after inserting one row
+     * @test
+     */
+    public function getUsers()
+    {
+        $this->assertEquals(\DB::table('users')->count(), 1);
+    }
+
     protected function getPackageProviders($app)
     {
         return [
