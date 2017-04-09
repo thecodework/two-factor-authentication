1c4cc9ea11a8888b81e67c7cfe9d049386a97b8d
Ashish Singh <imrealashu@gmail.com>
2017-04-09 21:51:48 +0530

    Now the token verifier screen using pincode-input, improves UI.

---
+++ resources/assets/js/index.js
@@ -0,0 +1,33 @@
+// import jquery from 'jquery'
+// require('jquery')
+require('./pincode-input.js')
+require('./../css/pincode-input.css')
+
+// global.$ = global.jQuery = require('jquery');
+
+// $('#totp_token').hide();
+// jQuery('#totp_token').pincodeInput({
+//     hidedigits: false,
+//     inputs: 6,
+//     complete: alert('hi')
+// })
+window.$('#totp_token').pincodeInput({
+
+  // 4 input boxes = code of 4 digits long
+  inputs:6,
+
+  // hide digits like password input
+  hideDigits:false,
+
+  // keyDown callback
+  keydown : function(e){},
+
+  // callback when all inputs are filled in (keyup event)
+  complete : function(value, e, errorElement){
+    // value = the entered code
+    // e = last keyup event
+    // errorElement = error span next to to this, fill with html
+    // e.g. : $(errorElement).html("Code not correct");
+  }
+
+});
--- resources/views/verify.blade.php
+++ resources/views/verify.blade.php
@@ -3,16 +3,15 @@
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Verify Two Factor Authentication</div>
+            <div class="panel panel-success">
+                <div class="panel-heading text-center">Verify Two Factor Authentication</div>
                 <div class="panel-body">
                     <form class="form-horizontal" role="form" method="POST" action="{{ url('/verify-2fa') }}">
                         {{ csrf_field() }}
-
+                        <div class="alert alert-warning text-center">Download the <strong>Google Authenticator</strong> App on your phone from the Play Store or the App Store.</div><br/>
                         <div class="form-group{{ $errors->has('totp_token') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">TOTP Token</label>
-
-                            <div class="col-md-6">
+                            <label for="totp_token" class="col-md-4 control-label">TOTP Token</label>
+                            <div class="col-md-6 pincode-input-container">
                                 <input id="totp_token" autocomplete="off" type="text" class="form-control" name="totp_token" value="{{ old('totp_token') }}" autofocus>
 
                                 @if ($errors->has('totp_token'))
@@ -35,4 +34,30 @@
         </div>
     </div>
 </div>
+<script type="text/javascript" src="{{asset('js/two-factor-authentication.js')}}"></script>
+{{-- <style>
+    .pincode-input-container {
+    display:inline-block;
+}
+.pincode-input-container input.first {
+    border-top-right-radius:0px;
+    border-bottom-right-radius:0px;
+}
+.pincode-input-container input.last {
+    border-top-left-radius:0px;
+    border-bottom-left-radius:0px;
+    border-left-width:0px;
+}
+.pincode-input-container input.mid {
+    border-radius:0px;
+    border-left-width:0px;
+}
+.pincode-input-text, .form-control.pincode-input-text {
+    width:35px;
+    float:left;
+}
+.pincode-input-error{
+    clear:both;
+}
+</style> --}}
 @endsection
